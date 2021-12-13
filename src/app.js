819b30124cd5389284fc2b27d8a0a9dce08dd0a9
daviddohru@gmail.com <qwerty12AS12AS>
2021-12-14 00:46:48 +0300

    solutions

--- src/app.js
+++ src/app.js
@@ -1,1 +1,46 @@
 'use strict';
+
+const readline = require('readline');
+const fs = require('fs');
+
+let fileName = '';
+let fileContent = '';
+
+const terminal = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout,
+});
+
+const question1 = () => {
+  return new Promise((resolve, reject) => {
+    terminal.question('enter the file name: ', (answer) => {
+      fileName = answer;
+      resolve();
+    });
+  });
+};
+
+const question2 = () => {
+  return new Promise((resolve, reject) => {
+    terminal.question('enter the content? ', (answer) => {
+      fileContent = answer;
+      resolve();
+    });
+  });
+};
+
+const main = async() => {
+  try {
+    await question1();
+    await question2();
+
+    fs.writeFileSync(`./folderForNewFile/${fileName}.txt`, fileContent);
+    // eslint-disable-next-line no-console
+    console.log('Success created file');
+  } catch (e) {
+    // eslint-disable-next-line no-console
+    console.log(e);
+  }
+};
+
+main();
